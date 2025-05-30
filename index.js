e5b09da76128d6f00d57a73cd8076a2d086ca457
Mert Eren Dilsiz <merteren@Mert-MacBook-Pro.local>
2025-05-30 18:54:59 +0300

    Yeni değişiklikler

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(session({
         maxAge: 24 * 60 * 60 * 1000 // 24 saat
     }
 }));
-
+//selam
 // Initialize Passport AFTER session middleware
 app.use(passport.initialize());
 app.use(passport.session());
